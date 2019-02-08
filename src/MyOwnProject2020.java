52e9eadf64c8889ecce7912393059958b7a4a670
Sayedkhater <khaterkhan@yahoo.com>
2019-02-08 16:16:33 -0500

    no body is perfect

---
+++ src/MyOwnProject2020.java
@@ -0,0 +1,6 @@
+public class MyOwnProject2020 {
+    public static void main(String[] args) {
+        System.out.println("No body is perfect" );
+        System.out.println("a;kd;sklfk" );
+    }
+}
---
+++ src/MyPro2019.java
@@ -0,0 +1,6 @@
+public class MyPro2019 {
+    public static void main(String[] args) {
+        System.out.println("the power is yours" );
+        System.out.println("you cant beat the feeling" );
+    }
+}
